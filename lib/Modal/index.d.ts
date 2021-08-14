ff6b3c091d1de1de85f4a3db41a4f9b853d5605f
ali <a.khadim@xpay.de>
2021-08-14 21:11:26 +0200

    Add lib folder

---
+++ lib/Modal/index.d.ts
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'wicg-inert';
+interface ModalProps {
+    /**
+     *  Where you can add the modal elements
+     */
+    children?: React.ReactNode;
+    /**
+     *  When it's true it will open the modal
+     */
+    open?: boolean;
+    /**
+     * Callback fired when the Modal is requested to be closed by a click on the overlay or when user press esc key
+     */
+    onClose?: () => void;
+    /**
+     * When it's true it will prevent close the modal when you click on the backdrop of the close button
+     */
+    locked?: boolean;
+    /**
+     * You can specify the parent of the modal where you can render the portal by default it will create a div
+     */
+    parent?: HTMLElement;
+    /**
+     * You can add class the the parent dev
+     */
+    parentClass?: string;
+}
+declare const Modal: React.FC<ModalProps>;
+export default Modal;
+//# sourceMappingURL=index.d.ts.map
