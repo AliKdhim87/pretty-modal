18a3a7df99385149476ff9b99e8d62bca68b202f
AliKdhim87 <alikadhim87nl@gmail.com>
2022-02-19 13:07:18 +0100

    fix(package.json, gitignore): update dependency
    - [x] add lib folder to gitignore
    - [x] exclude lib in the jest config

--- lib/portal/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface PortalProps {
-    children?: React.ReactNode;
-    parent?: string;
-    className?: string;
-}
-declare const Portal: React.FC<PortalProps>;
-export default Portal;
-//# sourceMappingURL=index.d.ts.map
